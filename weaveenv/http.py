529a19be0662875d5f0f4172ab52a7c6f6db5706
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2018-12-30 18:42:59 +0530

    Add test cases.

--- weaveenv/http.py
+++ weaveenv/http.py
@@ -1,16 +1,35 @@
 import json
 import logging
 import os
+from wsgiref.simple_server import make_server, WSGIRequestHandler
 
-from bottle import Bottle, static_file, request, response
+from bottle import Bottle, ServerAdapter, static_file, request, response
 
 
 logger = logging.getLogger(__name__)
 
 
+# From: https://stackoverflow.com/a/16056443/227884
+class MyWSGIRefServer(ServerAdapter):
+    server = None
+
+    def run(self, handler):
+        if self.quiet:
+            class QuietHandler(WSGIRequestHandler):
+                def log_request(*args, **kw): pass
+            self.options['handler_class'] = QuietHandler
+        self.server = make_server(self.host, self.port, handler, **self.options)
+        self.server.serve_forever()
+
+    def stop(self):
+        self.server.server_close()
+        self.server.shutdown()
+
+
 class WeaveHTTPServer(Bottle):
     def __init__(self, modules, static_path="static"):
         super().__init__()
+        self.server = None
         self.static_path = static_path
 
         self.route("/")(self.handle_root)
@@ -18,9 +37,17 @@ class WeaveHTTPServer(Bottle):
 
         for prefix, module in modules:
             for method, path_suffix, callback in module.get_registrations():
-                path = os.path.join("/api", prefix.lstrip("/"), path_suffix)
+                path = os.path.join("/api", prefix.lstrip("/"),
+                                    path_suffix.lstrip("/"))
                 logger.info("Registering: %s at %s", callback, path)
                 self.route(path, method)(self.handle_api(method, callback))
+
+    def run(self, host="", port=15000):
+        self.server = MyWSGIRefServer(host=host, port=port)
+        super().run(server=self.server)
+
+    def stop(self):
+        self.server.stop()
 
     def handle_root(self):
         return self.handle_static("/index.html")
@@ -38,11 +65,11 @@ class WeaveHTTPServer(Bottle):
             if method == "POST":
                 params = json.load(request.body)
             elif method == "GET":
-                params = request.forms
+                params = request.query
             try:
                 status_code, resp = callback(params)
                 return return_response(status_code, resp)
-            except Exception as e:
+            except Exception:
                 logger.exception("Internal server error.")
                 return return_response(500, {"error": "Internal Server Error."})
         return process_request
