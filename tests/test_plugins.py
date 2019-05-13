8186b98b1ea8495d5c492dd41dc462f6f4e08590
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2019-05-12 19:09:49 -0700

    Add test cases.

---
+++ tests/test_plugins.py
@@ -0,0 +1,111 @@
+import json
+import os
+import textwrap
+
+import pytest
+
+from weavelib.exceptions import PluginLoadError
+from weavelib.services import BasePlugin
+
+from weaveenv.plugins import load_plugin_json
+
+
+class TestPluginLoadJson(object):
+    @pytest.fixture(autouse=True)
+    def setup(self, tmpdir):
+        self.tmpdir = tmpdir.strpath
+        self.plugin_dir = os.path.join(self.tmpdir, "plugin")
+        os.makedirs(self.plugin_dir)
+
+    def write_content(self, path, contents):
+        with open(os.path.join(self.plugin_dir, path), "w") as out:
+            out.write(contents)
+
+    def test_no_plugin_json(self):
+        with pytest.raises(PluginLoadError) as ex:
+            load_plugin_json(self.plugin_dir)
+
+        assert ex.value.extra == "Error opening plugin.json."
+
+    def test_invalid_json(self):
+        self.write_content("plugin.json", "hello word.")
+
+        with pytest.raises(PluginLoadError) as ex:
+            load_plugin_json(self.plugin_dir)
+
+        assert ex.value.extra == "Error parsing plugin.json."
+
+    def test_no_service_field(self):
+        self.write_content("plugin.json", '{"hello": "world"}')
+        with pytest.raises(PluginLoadError) as ex:
+            load_plugin_json(self.plugin_dir)
+
+        assert ex.value.extra == "Required field not found in plugin.json."
+
+    def test_bad_service_specification(self):
+        self.write_content("plugin.json", '{"service": "hello world"}')
+        with pytest.raises(PluginLoadError) as ex:
+            load_plugin_json(self.plugin_dir)
+
+        assert ex.value.extra == "Bad 'service' specification in plugin.json."
+
+    def test_failing_import(self):
+        self.write_content("plugin.json", '{"service": "a.b.c"}')
+        with pytest.raises(PluginLoadError) as ex:
+            load_plugin_json(self.plugin_dir)
+
+        assert ex.value.extra == "Failed to import dependencies."
+
+    def test_not_base_plugin(self):
+        # This just tries to import from weaveenv
+        self.write_content("plugin.json",
+                           '{"service": "weaveenv.plugins.VirtualEnvManager"}')
+        with pytest.raises(PluginLoadError) as ex:
+            load_plugin_json(self.plugin_dir)
+
+        assert ex.value.extra == "Service must inherit BasePlugin."
+
+    def test_good_plugin_load(self):
+        spec = {
+            "service": "plugin.main.TestPlugin",
+            "config": {"hello": "world"},
+            "start_timeout": 10,
+            "deps": [1, 2, 4]
+        }
+        main_py = """
+        from weavelib.services import BasePlugin
+
+        class TestPlugin(BasePlugin):
+            pass
+        """
+        package_dir = os.path.join(self.plugin_dir, "plugin")
+        main_py_path = os.path.join(self.plugin_dir, "plugin", "main.py")
+
+        os.makedirs(self.plugin_dir + "/plugin")
+        self.write_content("plugin.json", json.dumps(spec))
+        self.write_content(main_py_path, textwrap.dedent(main_py))
+
+        expected = {
+            "deps": [1, 2, 4],
+            "package_path": "plugin.main.TestPlugin",
+            "config": {"hello": "world"},
+            "start_timeout": 10
+        }
+
+        plugin_info = load_plugin_json(self.plugin_dir)
+        plugin_class = plugin_info.pop("service_cls")
+
+        assert expected == plugin_info
+        assert plugin_class.__name__ == "TestPlugin"
+
+
+class TestVirtualEnvManager(object):
+    @pytest.fixture(autouse=True)
+    def setup(self, tmpdir):
+        self.tmpdir = tmpdir.strpath
+        self.plugin_dir = os.path.join(self.tmpdir, "plugin")
+        os.makedirs(self.plugin_dir)
+
+    def write_content(self, path, contents):
+        with open(os.path.join(self.plugin_dir, path), "w") as out:
+            out.write(contents)
